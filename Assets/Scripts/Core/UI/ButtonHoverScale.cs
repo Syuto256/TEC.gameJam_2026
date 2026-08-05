06673f8265aebd86477a7a51f242cb43aa6ff5ca
suzuki-30 <sba25j10004@edu.sba.ac.jp>
2026-08-06 04:55:22 +0900

    ボタンのホバー／押下演出の統一

--- Assets/Scripts/Core/UI/ButtonHoverScale.cs
+++ Assets/Scripts/Core/UI/ButtonHoverScale.cs
@@ -1,38 +1,93 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 /// <summary>
 /// カーソルが乗った時にボタンをスムーズに拡大/縮小するコンポーネント。
 /// </summary>
-public class ButtonHoverScale : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class ButtonHoverScale : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
 {
-    [Header("【拡大・アジリティ設定】")]
+    [Header("【拡大・押下設定】")]
     [Tooltip("ホバー時の拡大倍率")]
-    [SerializeField] private float hoverScale = 1.5f;
+    [SerializeField] private float hoverScale = 1.05f;
+
+    [Tooltip("押下中の縮小倍率")]
+    [SerializeField] private float pressScale = 0.95f;
 
     [Tooltip("拡大・縮小にかかる時間（秒）")]
     [SerializeField] private float duration = 0.1f;
 
     private Vector3 defaultScale;
     private Coroutine scaleCoroutine;
+    private Button button;
+    private bool isPointerOver;
+    private bool isPointerDown;
 
     private void Awake()
     {
         // 初期状態のスケールを保持
         defaultScale = transform.localScale;
+        button = GetComponent<Button>();
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        // カーソルが乗ったら 1.5 倍（指定倍率）へ
-        StartScaleAnimation(defaultScale * hoverScale);
+        isPointerOver = true;
+        UpdateScaleAnimation();
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        // カーソルが離れたら元のサイズへ
-        StartScaleAnimation(defaultScale);
+        isPointerOver = false;
+        UpdateScaleAnimation();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (!IsInteractable())
+        {
+            return;
+        }
+
+        isPointerDown = true;
+        UpdateScaleAnimation();
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (!IsInteractable())
+        {
+            return;
+        }
+
+        isPointerDown = false;
+        UpdateScaleAnimation();
+    }
+
+    private bool IsInteractable()
+    {
+        return button == null || button.interactable;
+    }
+
+    private void UpdateScaleAnimation()
+    {
+        if (!IsInteractable())
+        {
+            return;
+        }
+
+        var targetScale = defaultScale;
+        if (isPointerDown && isPointerOver)
+        {
+            targetScale = defaultScale * pressScale;
+        }
+        else if (isPointerOver)
+        {
+            targetScale = defaultScale * hoverScale;
+        }
+
+        StartScaleAnimation(targetScale);
     }
 
     private void StartScaleAnimation(Vector3 targetScale)
